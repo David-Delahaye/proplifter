7f8fe1cbaf527f86fb897983ac62900d08ab3715
David Delahaye <delahaye3@hotmail.co.uk>
2021-01-08 18:14:09 +0000

    hook up profile routes

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,14 @@
 import { useAuth } from "@/lib/auth";
 import { createPlant } from "@/lib/db";
+import { Text, Heading } from "@chakra-ui/react";
 
 export default function Home() {
   const auth = useAuth();
 
   return (
     <>
-      <h1>Welcome to proplifter</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createPlant({
-            name: e.target.name.value,
-            height: e.target.height.value,
-            authorId: auth?.user?.uid,
-          });
-        }}
-      >
-        <input name="name" placeholder="name" />
-        <input name="height" placeholder="12cm" />
-        <button>Add a plant</button>
-      </form>
+      <Heading>Welcome to proplifter</Heading>
+      <Text>some interesting facts about product, general marketing shite</Text>
     </>
   );
 }
--- pages/plants/index.js
+++ pages/plants/index.js
@@ -1,6 +1,10 @@
 import { useAuth } from "@/lib/auth";
+import { createPlant } from "@/lib/db";
 import fetcher from "@/utils/fetcher";
-import useSWR from "swr";
+import next from "next";
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import useSWR, { mutate } from "swr";
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -12,9 +16,36 @@ export default function Dashboard() {
     return (
       <>
         <h1>Dashboard</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const newPlant = {
+              name: e.target.name.value,
+              height: e.target.height.value,
+              authorId: user?.uid,
+            };
+            const { id } = createPlant(newPlant);
+
+            mutate(
+              ["/api/plants", user.token],
+              async (data) => ({
+                plants: [{ id, ...newPlant }, ...data.plants],
+              }),
+              false
+            );
+          }}
+        >
+          <input name="name" placeholder="name" />
+          <input name="height" placeholder="12cm" />
+          <button>Add a plant</button>
+        </form>
+
         {plants.map((plant) => (
           <li key={plant.id}>
-            {plant.name} - {plant.height}
+            {plant.name} - {plant.height} -
+            <NextLink href={`/plants/${plant.id}`}>
+              <Link>See more</Link>
+            </NextLink>
           </li>
         ))}
       </>
