8b7c415054cd7b7a3f897d29b7db6d2802681cfb
David Delahaye <delahaye3@hotmail.co.uk>
2021-01-10 02:59:29 +0000

    add basics to plant profile

--- pages/plants/[plant].js
+++ pages/plants/[plant].js
@@ -1,4 +1,6 @@
+import PlantIcon from "@/components/PlantIcon";
 import { getAllPlants, getPlant } from "@/lib/db-admin";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 
 export async function getStaticPaths() {
   const { plants } = await getAllPlants();
@@ -29,9 +31,15 @@ export async function getStaticProps(context) {
 export default function Profile({ plant }) {
   return (
     <>
-      <h1>
-        PROFILE PAGE === {plant.name} - {plant.height}
-      </h1>
+      <Flex>
+        <PlantIcon icon={plant.icon} />
+        <Heading>{plant.name}</Heading>
+        <Flex flexDirection="column">
+          <Text>height: {plant.height}</Text>
+          <Text>water schedule: {plant.water}</Text>
+          <Text>sunlight Preference: {plant.sunlight}</Text>
+        </Flex>
+      </Flex>
     </>
   );
 }
--- pages/plants/index.js
+++ pages/plants/index.js
@@ -2,10 +2,11 @@ import { useAuth } from "@/lib/auth";
 import { createPlant } from "@/lib/db";
 import fetcher from "@/utils/fetcher";
 import next from "next";
-import { Link } from "@chakra-ui/react";
+import { Flex, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import useSWR, { mutate } from "swr";
 import AddPlantModal from "@/components/AddPlantModal";
+import PlantIcon from "@/components/PlantIcon";
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -18,12 +19,13 @@ export default function Dashboard() {
         <h1>Dashboard</h1>
         <AddPlantModal />
         {plants.map((plant) => (
-          <li key={plant.id}>
-            {plant.name} - {plant.height} -
+          <Flex key={plant.id} alignItems="center" m={2}>
+            <PlantIcon icon={plant.icon} />
+            <Text>{plant.name}</Text>
             <NextLink href={`/plants/${plant.id}`}>
               <Link>See more</Link>
             </NextLink>
-          </li>
+          </Flex>
         ))}
       </>
     );
