04a2e032ca92cea64f87c27cddf48b7e0a23e84d
David Delahaye <delahaye3@hotmail.co.uk>
2021-01-15 23:03:48 +0000

    fix up quickUpdates

--- components/QuickUpdate.js
+++ components/QuickUpdate.js
@@ -1,9 +1,31 @@
 import { updatePlant } from "@/lib/db";
-import { Link, Text } from "@chakra-ui/react";
-import { isToday, parseISO } from "date-fns";
+import { Nutrition, Tick, Water } from "@/styles/icons";
+import { Link, Text, Tooltip } from "@chakra-ui/react";
+import {
+  addDays,
+  differenceInCalendarDays,
+  formatDistance,
+  formatDistanceToNow,
+  isToday,
+  parse,
+  parseISO,
+} from "date-fns";
 
 export function QuickWater({ plant }) {
-  if (isToday(parseISO(plant.lastWatered))) return <Text>Ticked</Text>;
+  const nextWater = addDays(parseISO(plant.lastWatered), plant.water);
+  const tillNextWater = formatDistanceToNow(nextWater);
+  if (isToday(parseISO(plant.lastWatered))) {
+    return (
+      <Tooltip
+        label="Already Watered Today"
+        aria-label="A tooltip"
+        bg="main.900"
+        color="back.900"
+      >
+        <Tick boxSize={12} />
+      </Tooltip>
+    );
+  }
   return (
     <>
       <Link
@@ -13,14 +35,29 @@ export function QuickWater({ plant }) {
           });
         }}
       >
-        Water
+        <Water boxSize={12} />
       </Link>
     </>
   );
 }
 
 export function QuickFeed({ plant }) {
-  if (isToday(parseISO(plant.lastFed))) return <Text>Ticked</Text>;
+  if (isToday(parseISO(plant.lastFed))) {
+    return (
+      <>
+        <Tooltip
+          label="Already Fed Today"
+          aria-label="A tooltip"
+          bg="main.900"
+          color="back.900"
+        >
+          <Tick boxSize={12} />
+        </Tooltip>
+      </>
+    );
+  }
+  const nextFeed = addDays(parseISO(plant.lastFed), plant.feed);
+  const tillNextFeed = formatDistanceToNow(nextFeed);
   return (
     <>
       <Link
@@ -30,7 +67,7 @@ export function QuickFeed({ plant }) {
           });
         }}
       >
-        Feed
+        <Nutrition boxSize={12} />
       </Link>
     </>
   );
