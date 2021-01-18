00fa93b44f8114610128be82afa53e930ff761cd
David Delahaye <delahaye3@hotmail.co.uk>
2021-01-18 23:39:15 +0000

    add order plant by water

--- components/PlantTable.js
+++ components/PlantTable.js
@@ -1,7 +1,19 @@
 import { Flex } from "@chakra-ui/react";
+import {
+  addDays,
+  compareAsc,
+  compareDesc,
+  formatDistanceToNow,
+  parseISO,
+} from "date-fns";
 import PlantRow from "./PlantRow";
 
 export default function PlantTable({ plants }) {
+  let list = plants;
+  plants.sort(function (a, b) {
+    return compareAsc(parseISO(a.nextWater), parseISO(b.nextWater));
+  });
+
   return (
     <>
       <Flex flexDir="column">
