1613c5e6cd5fd7be817c0097eade98659dba0f2e
David Delahaye <delahaye3@hotmail.co.uk>
2021-01-27 21:20:03 +0000

    add login signup ui

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,90 @@
+import DashboardShell from "@/components/DashboardShell";
+import PageRow from "@/components/PageRow";
+import PlantIcon from "@/components/PlantIcon";
 import { useAuth } from "@/lib/auth";
-import { Button, Heading } from "@chakra-ui/react";
+import { Google } from "@/styles/icons";
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  Link,
+  Center,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import NextLink from "next/link";
+import { useState } from "react";
+
+function PasswordInput(props) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <InputGroup size="md" {...props}>
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="Password"
+        backgroundColor="back.900"
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+}
 
 export default function Login() {
   const auth = useAuth();
   const router = useRouter();
   if (auth.user) router.push("/account");
   return (
-    <>
-      <Heading>Login</Heading>
-      <Button
-        variant="solid"
-        size="md"
-        backgroundColor="main.900"
-        color="back.900"
-        onClick={(e) => {
-          auth.signinWithGoogle();
-        }}
-      >
-        Login With Google
-      </Button>
-    </>
+    <DashboardShell>
+      <PageRow>
+        <Flex flexDir="column" justifyContent="center" flex="1">
+          <Heading mb={8}>Login</Heading>
+          <Input placeholder="Email" backgroundColor="back.900" mb={2} />
+          <PasswordInput mb={8} />
+          <Button
+            variant="solid"
+            size="md"
+            backgroundColor="main.900"
+            color="back.900"
+            mb={2}
+          >
+            Login
+          </Button>
+          <Text fontWeight="light">
+            Dont have an Account?{" "}
+            <NextLink href="/signup">
+              <Link>Sign Up</Link>
+            </NextLink>
+          </Text>
+          <Center h={20}>OR</Center>
+          <Button
+            variant="solid"
+            size="md"
+            backgroundColor="main.900"
+            color="back.900"
+            onClick={(e) => {
+              auth.signinWithGoogle();
+            }}
+          >
+            <Google boxSize={6} mr={4} />
+            Login With Google
+          </Button>
+        </Flex>
+        <PlantIcon
+          icon={4}
+          display={["none", "none", "none", "block"]}
+          flex="1"
+        />
+      </PageRow>
+    </DashboardShell>
   );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,22 @@
 import { useAuth } from "@/lib/auth";
-import { Button, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+
+import { Google } from "@/styles/icons";
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  Link,
+  Center,
+} from "@chakra-ui/react";
+import DashboardShell from "@/components/DashboardShell";
+import PageRow from "@/components/PageRow";
+import NextLink from "next/link";
+import PlantIcon from "@/components/PlantIcon";
 
 export default function SignUp() {
   const router = useRouter();
@@ -8,20 +24,53 @@ export default function SignUp() {
   if (auth.user) router.push("/account");
 
   return (
-    <>
-      <Heading>Sign Up</Heading>
-      Signup OR
-      <Button
-        variant="solid"
-        size="md"
-        backgroundColor="main.900"
-        color="back.900"
-        onClick={(e) => {
-          auth.signinWithGoogle();
-        }}
-      >
-        Login With Google
-      </Button>
-    </>
+    <DashboardShell>
+      <PageRow>
+        <Flex flexDir="column" justifyContent="center" flex="1">
+          <Heading mb={8}>Sign Up</Heading>
+          <Input placeholder="Email" backgroundColor="back.900" mb={2} />
+          <Input placeholder="Display Name" backgroundColor="back.900" mb={8} />
+          <Input placeholder="Password" backgroundColor="back.900" mb={2} />
+          <Input
+            placeholder="Confirm your password"
+            backgroundColor="back.900"
+            mb={8}
+          />
+          <Button
+            variant="solid"
+            size="md"
+            backgroundColor="main.900"
+            color="back.900"
+            mb={2}
+          >
+            Create An Account
+          </Button>
+          <Text fontWeight="light">
+            Already have an Account?{" "}
+            <NextLink href="/login">
+              <Link>Login</Link>
+            </NextLink>
+          </Text>
+          <Center h={20}>OR</Center>
+          <Button
+            variant="solid"
+            size="md"
+            backgroundColor="main.900"
+            color="back.900"
+            onClick={(e) => {
+              auth.signinWithGoogle();
+            }}
+          >
+            <Google boxSize={6} mr={4} />
+            Login With Google
+          </Button>
+        </Flex>
+        <PlantIcon
+          icon={32}
+          display={["none", "none", "none", "block"]}
+          flex="1"
+        />
+      </PageRow>
+    </DashboardShell>
   );
 }
